36117cc4fd7c8ef07fd117b73175b3edf6e9cceb
Bryce Milbank <student.bryce@hotmail.com>
2025-03-08 03:15:28 +1300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SCP_json_react_app/',
+  base: '/SCP_react_app/',
 })
